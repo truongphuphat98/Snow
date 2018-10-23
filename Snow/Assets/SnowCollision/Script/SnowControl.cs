b9efa4c5b800b5315fc357c5721854509cc644e2
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2018-10-23 16:22:58 +0200

    Add Snow Project

---
+++ Snow/Assets/SnowCollision/Script/SnowControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SnowControl : MonoBehaviour {
+
+    public Slider timeRank;
+    [HideInInspector]
+    public float time = 0.0f;
+
+    public void ClearSnow()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void UpdateTime()
+    {
+        time = timeRank.value;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
